0c4006667f1bd82b53aff0afae9711ec3ed85e3d
Edgars <edgars.avenins@gmail.com>
2019-09-20 16:30:13 +1200

    Complete challenge add button with click event

--- classes.js
+++ classes.js
@@ -5,21 +5,43 @@ document.addEventListener('DOMContentLoaded', start)
 
 function start () {
   // The first example is done for you. Uncomment the line below and reload the browser.
-  // one()
-    
+  //one();
+  two();
+  three();
+  makeVisible();
   // Your turn! Create a new function called `two`, then call it from here.
 }
 
+
 function one () {
   // First, we have to find the element:
-  var one = document.getElementById('one')
+  var one = document.getElementById('one');
+    one.classList.toggle('blue')
+    //one.classList.add('blue');
 
   // Next, we apply a new CSS class to it:
-  one.classList.add('blue')
+  
 }
 
 // CREATE FUNCTION two HERE
-
+function two(){
+  var two = document.getElementById('two');
+  two.classList.add('green');
+}
 // CREATE FUNCTION three HERE
-
+function three(){
+  var three = document.getElementById('three');
+  three.classList.add('red');
+}
 // CREATE FUNCTION makeVisible HERE
+function makeVisible(){
+  var visInvis = document.getElementsByClassName('invisible')[0];
+  visInvis.classList.add('visible');
+}
+
+
+document.getElementById('click1').addEventListener("click", one);
+//document.getElementById('click1').onclick = one; OK
+/* var x = document.getElementById('click1');
+    x.addEventListener('onclick', one); // NOP???
+*/
